fe735fc89bf51af0550c4750584f5fecb30b1ade
Sabogdal <thomasgs.91@gmail.com>
2025-09-26 17:09:15 +0200

    Fikset absolutt alt

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,36 +2,63 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
-	// a)
-	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-	}
+    // a)
+    public static void skrivUt(int[][] matrise) {
+
+        for (int[] rad : matrise) {
+            for (int plass : rad) {
+                System.out.print(plass + " ");
+            }
+        }
+        System.out.println();
+    }
+
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+        String streng = "";
+
+        for (int [] rad: matrise) {
+            for (int plass : rad){
+                streng += plass + " ";
+            }
+            streng += "\n";
+        }
+        return streng;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
-	}
+        int[][] nyMatrise = new int [matrise.length][matrise[0].length];
+
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[0].length; j++) {
+                nyMatrise[i][j] = matrise[i][j]* tall;
+            }
+        }
+        return nyMatrise;
+    }
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
-	}
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < b.length; i++) {
+            if (a[i].length != b[i].length) {
+                return false;
+            }
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+    return true;
+    }
+
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
