e22eda31e8fee5a0d4ae0b9f8ac279d67aeeb586
Sabogdal <thomasgs.91@gmail.com>
2025-09-26 13:44:43 +0200

    Fikset alt

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -18,50 +18,78 @@ public class Tabeller {
 	// b) Ferdig
 
 	public static String tilStreng(int[] tabell) {
-        System.out.print("[");
-        String tabellRetur = "";
-        for (int i = 0; i<tabell.length; i++){
-            if (i < tabell.length-1) {
-                tabellRetur = (tabellRetur + tabell[i] + ", ");
+
+        String tabellRetur = "[";
+        for (int i = 0; i < tabell.length; i++) {
+
+            if (i == tabell.length-1) {
+            tabellRetur += (tabell[i]);
             }
-                else {
-                tabellRetur = (tabellRetur + tabell[i] + "]");
-                }
-            System.out.println();
+
+            else {
+                tabellRetur += (tabell[i] + ",");
+            }
         }
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+        tabellRetur += "]";
+        return tabellRetur;
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
+        int sum = 0;
+        for (int i = 0; i <tabell.length; i++) {
+            sum += tabell[i];
+        }
+        return sum;
 
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+        boolean finnes = false;
+        for (int i = 0; i < tabell.length; i++) {
+            if (tabell[i] == tall) {
+                finnes = true;
+            }
+        }
+        return finnes;
+    }
+        //som gitt en tabell og et tall returnerer true
+        // om tabellen tabell innholder tallet tall og false ellers.
 
-	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
-	}
+        int finnes = -1;
+
+        for (int i = 0; i < tabell.length; i++) {
+            if (tabell[i] == tall) {
+                finnes = i;
+                break;
+            }
+        }
+        return finnes;
+    }
+
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
-	}
+        int [] nyTabell = new int [tabell.length];
+        int temp = tabell.length-1;
+        for (int i = 0; i < tabell.length; i++) {
+            nyTabell[temp] = tabell[i];
+            temp -= 1;
+
+            }
+        return nyTabell;
+        }
+
+
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
